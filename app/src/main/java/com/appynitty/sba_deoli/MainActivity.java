1a234087483dce6692f06058d55d902957a167cc
ayan <ayand@appynitty.com>
2019-08-27 19:04:39 +0530

    - update

--- app/src/main/java/com/appynitty/sba_deoli/MainActivity.java
+++ app/src/main/java/com/appynitty/sba_deoli/MainActivity.java
@@ -7,8 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.appynitty.swachbharatabhiyanlibrary.activity.SplashScreenActivity;
 import com.appynitty.swachbharatabhiyanlibrary.utils.AUtils;
-
-import quickutils.core.QuickUtils;
+import com.pixplicity.easyprefs.library.Prefs;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        QuickUtils.prefs.save(AUtils.APP_ID, "1");
-        QuickUtils.prefs.save(AUtils.VERSION_CODE, BuildConfig.VERSION_CODE);
+        Prefs.putString(AUtils.APP_ID, "1");
+        Prefs.putInt(AUtils.VERSION_CODE, BuildConfig.VERSION_CODE);
 
         startActivity(new Intent(MainActivity.this, SplashScreenActivity.class));
     }
