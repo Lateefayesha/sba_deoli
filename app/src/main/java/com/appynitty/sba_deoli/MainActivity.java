8363e20d484d0d5bf18647c1acb7eadf5f2573e6
ayesha <appynitty.dev@gmail.com>
2020-12-04 12:09:14 +0530

    Merge branch 'master' of https://bitbucket.org/appynitty/sba_deoli
    # Conflicts:
    #	app/build.gradle
    #	app/libs/swachbharatabhiyanlibrary-release.aar
    #	build.gradle
    #	gradle/wrapper/gradle-wrapper.properties

--- app/src/main/java/com/appynitty/sba_deoli/MainActivity.java
+++ app/src/main/java/com/appynitty/sba_deoli/MainActivity.java
@@ -2,15 +2,12 @@ package com.appynitty.sba_deoli;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.appynitty.swachbharatabhiyanlibrary.activity.SplashScreenActivity;
-import com.appynitty.swachbharatabhiyanlibrary.pojos.LanguagePojo;
 import com.appynitty.swachbharatabhiyanlibrary.utils.AUtils;
 import com.pixplicity.easyprefs.library.Prefs;
+import leakcanary.LeakCanary;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if(BuildConfig.DEBUG)
+          createLeakConfig();
         Prefs.putString(AUtils.APP_ID, "1");
         Prefs.putInt(AUtils.VERSION_CODE, BuildConfig.VERSION_CODE);
-
         startActivity(new Intent(MainActivity.this, SplashScreenActivity.class));
+
     }
+
+        private void createLeakConfig() {
+        LeakCanary.Config config = LeakCanary.getConfig().newBuilder()
+                .dumpHeap(true)
+                .dumpHeapWhenDebugging(true)
+                .computeRetainedHeapSize(true)
+                .build();
+        LeakCanary.setConfig(config);
+
+
+    }
+
+
 
     @Override
     protected void onPause() {
         super.onPause();
         finish();
+
     }
+
+
+
 }
